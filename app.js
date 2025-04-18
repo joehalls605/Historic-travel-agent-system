3a8e7391131ec5de197568dc1d24d199bdb6aa20
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-04-18 13:32:17 +0100

    filtering logic set up.

--- app.js
+++ app.js
@@ -2,30 +2,55 @@ import locationData from './location-data.js';
 
 
 const displayedLocations = document.getElementById("displayedLocations");
+const countryOptions = document.getElementById("countryOptions");
 
-// DISPLAY THE LOCATIONS
 document.addEventListener("DOMContentLoaded", function(){
     if(displayedLocations){
-        locationData.locations.forEach(item => {
-            const locationDiv = document.createElement("div");
-            locationDiv.innerHTML = `
-            <div class="location-card">
-            <img class="location-card-image" alt="image" src="${item.image}">
-            <div class="card-content">
-            <h3>${item.name}</h3>        
-            <p>${item.country}</p>
-            <p>${item.description}</p>   
-            <p>${item.significance}</p>
-            <p>${item.details}</p>
-            <div class="card-footer">
-            <p>£${item.price}</p>   
-            <p>Rating:${item.rating}</p>  
-            </div>
-            </div>
-            <button class="book-button">Book</button>
-            </div>
-            `;
-            displayedLocations.appendChild(locationDiv);
-        })
+        displayLocations(locationData.locations);
     }
 })
+
+function displayLocations(locationsArray){
+    displayedLocations.innerHTML = "";
+
+    locationsArray.forEach(item => {
+        const locationDiv = document.createElement("div");
+        locationDiv.innerHTML = `
+        <div class="location-card">
+        <img class="location-card-image" alt="image" src="${item.image}">
+        <div class="card-content">
+        <h3>${item.name}</h3>        
+        <p>${item.country}</p>
+        <p>${item.description}</p>   
+        <p>${item.significance}</p>
+        <p>${item.details}</p>
+        <div class="card-footer">
+        <p>£${item.price}</p>   
+        <p>Rating:${item.rating}</p>  
+        </div>
+        </div>
+        <button class="book-button">Book</button>
+        </div>
+        `;
+        displayedLocations.appendChild(locationDiv);
+    });
+}
+
+const applyFiltersButton = document.getElementById("filterButton");
+applyFiltersButton.addEventListener("click", applyFilters);
+
+function applyFilters(){
+    const filteredLocations = filterDisplayedLocations();
+    displayLocations(filteredLocations);
+}
+
+function filterDisplayedLocations(){
+    const selectedCountry = countryOptions.value;
+
+    return locationData.locations.filter(function(item){
+        if(item.country !== selectedCountry){ // removes it from the new array
+            return false;
+        }
+        return true;
+    });
+}
