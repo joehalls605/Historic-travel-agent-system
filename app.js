60f54ed679bd90ce1d71d835c620f4831ad538c2
Joseph Halls <46379660+joehalls605@users.noreply.github.com>
2025-06-14 10:06:58 +0100

    Mobile styling, and sidebar collapse added.

--- app.js
+++ app.js
@@ -148,6 +148,35 @@ function handleBook(event){
     }
 }
 
+// BURGER MENU
+const burger = document.getElementById("burger-menu");
+const sidebar = document.getElementById("sidebar");
+
+let overlay = document.createElement("div");
+overlay.id = "sidebar-overlay";
+document.body.appendChild(overlay);
+
+burger.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+    sidebar.classList.remove("open");
+    overlay.classList.remove("active");
+});
+
+// CLOSE BUTTON
+
+const closeBtn = document.getElementById("sidebar-close");
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.remove("open");
+    overlay.classList.remove("active");
+});
+
+
+
+
 
 
 
